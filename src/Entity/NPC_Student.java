6dce43082058e112d061ed1ee8eeae0f4a2a0156
Vinh Lê Đào Thành <ledaothanhvinh2002@gmail.com>
2022-05-28 11:27:20 +0700

    npc change

--- src/Entity/NPC_Student.java
+++ src/Entity/NPC_Student.java
@@ -3,23 +3,54 @@ package Entity;
 import Main.GamePanel;
 import javax.imageio.ImageIO;
 import java.io.IOException;
+import java.util.Random;
 
 
 public class NPC_Student extends Entity{
     public NPC_Student(GamePanel gp){
         super(gp);
-        direction ="up";
+        direction ="down";
         speed=2;
         setImage();
 
     }
     public void setImage() {
         try {
-            up1 = ImageIO.read(getClass().getResourceAsStream("/Student-clone/Student_clone.png"));
+            up1 = ImageIO.read(getClass().getResourceAsStream("/Student/NPC_up1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/Student/NPC_up2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/Student/Npc_down1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/Student/Npc_down2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/Student/NPC_right1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/Student/NPC_right2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/Student/NPC_left1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/Student/NPC_left2.png"));
+
 
         } catch(IOException e) {
             e.printStackTrace();
         }
     }
+    public void setAction(){
+        actionLockCounter++;
+        if(actionLockCounter==200){
+        Random random = new Random();
+        int i= random.nextInt(100)+1;// pick a number for running
+        if(i<=25){
+            direction="up";
+        }
+        if(i>25 && i<+50){
+            direction="down";
+        }
+        if(i>50 && i<=75) {
+            direction="left";
+        }
+        if(i>75 && i<=100) {
+            direction="right";
+        }
+        actionLockCounter=0;
+        }
+        }
 
-}
+    }
+
+
