d0017cc5a16f78e0342b55f9d5c3199fb5793b0a
KhoaVo128 <buttskicker128@gmail.com>
2022-06-01 09:29:05 +0700

    fixed 10 to5

--- src/object/OBJ_Student_Desk_Right.java
+++ src/object/OBJ_Student_Desk_Right.java
@@ -3,9 +3,6 @@ package object;
 import Entity.Entity;
 import Main.GamePanel;
 
-import javax.imageio.ImageIO;
-import java.io.IOException;
-
 public class OBJ_Student_Desk_Right extends Entity {
     public OBJ_Student_Desk_Right(GamePanel gp){
         super(gp);
